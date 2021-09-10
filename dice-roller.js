58fcd97d8a0d09f1b75fb12897a1462ac45d3c95
Erivan Ferreira <erivan.c39@gmail.com>
2021-09-10 08:45:41 -0300

    adicionar script roller

---
+++ dice-roller.js
@@ -0,0 +1,45 @@
+const lista=document.getElementById('Lista')
+const frequencia = [0,0,0,0,0,0,0,0,0,0,0]
+for(let i=0;i<1000;i++){
+    let roll = rolaDado()+rolaDado()
+    frequencia[roll-2]+=1
+}
+console.log(frequencia)
+let maiorFrequencia=0
+for (let i=0; i<frequencia.length; i++){
+    if(frequencia[i]>maiorFrequencia){
+        maiorFrequencia=frequencia[i]
+    }
+}
+console.log(maiorFrequencia)
+const frequenciaNormal=[]//armazena a frequência em porcentagem da maior frequência
+for(let i=0;i<frequencia.length;i++){
+    let porcentagem=frequencia[i]*100/maiorFrequencia
+    console.log(porcentagem)
+    frequenciaNormal.push(porcentagem+'%')
+}
+console.log(frequenciaNormal)
+for(let i=0;i<frequencia.length;i++){
+    lista.appendChild(criarElemento(i+2,frequencia[i],frequenciaNormal[i]))
+}
+
+function criarElemento(indice,n,tamanho){
+    const item=document.createElement('li')
+    const ind=document.createElement('span')
+    ind.innerText=indice
+    const barra=document.createElement('div')
+    barra.innerText=n
+    barra.style.width=tamanho
+    item.appendChild(ind)
+    item.appendChild(barra)
+    return item
+}
+function rekt(comp){//cria e retorna uma div com um determinado comprimento
+    const rect=document.createElement('div')
+    rect.style.width = comp
+    return rect 
+}
+function rolaDado(){
+    res = Math.floor(1+Math.random()*6)
+    return res
+}
